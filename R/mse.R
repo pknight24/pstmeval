a5c332a827266d9cc62d18059aaa1fd86f805b84
Parker Knight <plb.knight@gmail.com>
2020-07-09 15:01:57 -0400

    Add function for mae

--- R/mse.R
+++ R/mse.R
@@ -1,4 +1,4 @@
-#' Compute the MSE between pseudotime estimates and true timepoints.
+#' Compute the mean squared error between pseudotime estimates and true timepoints.
 #'
 #' @param pseudotime An n-vector of pseudotime estimates.
 #' @param truetime An n-vector of true timepoint values.
@@ -8,4 +8,19 @@ mse <- function(pseudotime, truetime)
 {
   if (cor(pseudotime, truetime, method="kendall") < 0) pseudotime <- rev(pseudotime)
   sum((pseudotime - truetime)^2) / length(truetime)
+}
+
+
+#' Compute the mean absolute error between pseudotime estimates and true timepoints.
+#'
+#' @param pseudotime An n-vector of pseudotime estimates.
+#' @param truetime An n-vector of true timepoint values.
+#' @return The MAE between pseudotime and truetime.
+#' @export
+
+mae <- function(pseudotime, truetime)
+{
+  if (cor(pseudotime, truetime, method="kendall") < 0) pseudotime <- rev(pseudotime)
+  sum(abs(pseudotime - truetime)) / length(truetime)
+
 }
